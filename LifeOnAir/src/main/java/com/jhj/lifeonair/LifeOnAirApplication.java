8e52b9107884c777b771778a3ab078dbaaf90642
CITYPOPER <103362619+CITYPOPER@users.noreply.github.com>
2023-10-08 15:25:50 +0900

    First Commit

---
+++ LifeOnAir/src/main/java/com/jhj/lifeonair/LifeOnAirApplication.java
@@ -0,0 +1,13 @@
+package com.jhj.lifeonair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeOnAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeOnAirApplication.class, args);
+	}
+
+}
---
+++ LifeOnAir/src/test/java/com/jhj/lifeonair/LifeOnAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhj.lifeonair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeOnAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
