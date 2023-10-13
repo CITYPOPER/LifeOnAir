887f3a74bd64b1123ad3d9b3a68a252491269d4b
CITYPOPER <103362619+CITYPOPER@users.noreply.github.com>
2023-10-13 16:14:44 +0900

    Setting: user 구조, BusinessException 구조

--- LifeOnAir/src/main/java/com/jhj/lifeonair/domain/user/controller/UserController.java
+++ LifeOnAir/src/main/java/com/jhj/lifeonair/domain/user/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.jhj.lifeonair.domain.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jhj.lifeonair.domain.user.dto.UserRegisterRequestDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -10,4 +19,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/user")
 public class UserController {
 	
+	@Operation(summary = "유저 기본 정보 입력 요청")
+	@ApiResponse(responseCode = "201", description = "요청 성공")
+	@ApiResponse(responseCode = "404", description = "해당 유저가 없는 상태일 때")
+	@PatchMapping("/join")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void registerUser(@RequestBody @Valid UserRegisterRequestDto requestDto) {
+		
+	}
 }
--- LifeOnAir/src/main/java/com/jhj/lifeonair/domain/user/exception/repository/UserRepository.java
+++ LifeOnAir/src/main/java/com/jhj/lifeonair/domain/user/exception/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.jhj.lifeonair.domain.user.exception.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jhj.lifeonair.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Optional<User> findByUserId(Long userId);
 }
